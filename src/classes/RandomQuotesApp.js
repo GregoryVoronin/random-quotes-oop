11568ca28db01217238df73da780e9de007a6f41
GregoryVoronin <gregoryvoronin@gmail.com>
2026-02-17 14:30:01 +0100

    initial version using oop

---
+++ src/classes/RandomQuotesApp.js
@@ -0,0 +1,31 @@
+import RandomQuote from "./RandomQuote.js";
+
+class RandomQuotesApp {
+    constructor() {
+        this.randomQuoteBtn = document.getElementById("random-quote-btn");
+        this.quoteTextElement = document.getElementById("quote-text");
+        this.quoteAuthorElement = document.getElementById("quote-author");
+        this.currentQuote = {};
+
+        this.init();
+    }
+
+    displayCurrentQuote() {
+        const { text, author } = this.currentQuote;
+        this.quoteTextElement.textContent = `"${text}"`;
+        this.quoteAuthorElement.textContent = author;
+    }
+
+    getRandomeQuote() {
+        const randomQuote = RandomQuote.getRandomQuote();
+        this.currentQuote = randomQuote;
+        this.displayCurrentQuote();
+    }
+    init() {
+        this.randomQuoteBtn.addEventListener("click", () => {
+            this.getRandomeQuote();
+        });
+    }
+}
+
+export default RandomQuotesApp;
